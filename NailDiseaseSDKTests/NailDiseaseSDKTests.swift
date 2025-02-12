7f6cdce7c72e5da126076706b439e21f3e347e25
RMJP Kumara <piumalkumara@live.com>
2025-02-12 10:31:33 +0530

    project init

---
+++ NailDiseaseSDKTests/NailDiseaseSDKTests.swift
@@ -0,0 +1,17 @@
+//
+//  NailDiseaseSDKTests.swift
+//  NailDiseaseSDKTests
+//
+//  Created by Piumal Kumara on 2025-02-12.
+//
+
+import Testing
+@testable import NailDiseaseSDK
+
+struct NailDiseaseSDKTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
