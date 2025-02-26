65131de372fd3bd9c69e4e044b59ed1d167a8fcf
RMJP Kumara <piumalkumara@live.com>
2025-02-26 14:30:02 +0530

    sandbox error fixed

---
+++ NailDiseaseSDK/Sources/Services/TFLiteModelHandler.swift
@@ -0,0 +1,18 @@
+//
+//  Untitled.swift
+//  NailDiseaseSDK
+//
+//  Created by Piumal Kumara on 2025-02-12.
+//
+
+import TensorFlowLite
+import Foundation
+
+import TensorFlowLite
+import Foundation
+
+public class TFLiteTest {
+    public static func testPrint() {
+        print("✅ TensorFlow Lite is integrated successfully!")
+    }
+}
