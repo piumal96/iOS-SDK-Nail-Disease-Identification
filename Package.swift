90d8d28003e464e5c84760c3e90f6d2c2b55f834
licensespring <piumalkumara@live.com>
2025-03-10 22:46:04 +0530

    added spm code

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version:5.9
+import PackageDescription
+
+let package = Package(
+    name: "NailDiseaseSDK",
+    platforms: [
+        .iOS(.v14)  
+    ],
+    products: [
+        .library(
+            name: "NailDiseaseSDK",
+            targets: ["NailDiseaseSDK"]
+        ),
+    ],
+    dependencies: [
+        
+    ],
+    targets: [
+        .target(
+            name: "NailDiseaseSDK",
+            dependencies: [],
+            path: "Sources"  /
+        ),
+        .testTarget(
+            name: "NailDiseaseSDKTests",
+            dependencies: ["NailDiseaseSDK"],
+            path: "Tests"
+        ),
+    ]
+)
