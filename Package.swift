3764ceb64524da5e2fa0f42c9c65f77c678e53c6
RMJP KUMARA <piumalkumara@live.com>
2025-03-10 23:29:06 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "NailDiseaseSDK",
     platforms: [
-        .iOS(.v14)  
+        .iOS(.v13)  
     ],
     products: [
         .library(
