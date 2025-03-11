1f26b24d2ff30ea69f799fc1afeae463698746fb
RMJP KUMARA <piumalkumara@live.com>
2025-03-11 20:07:01 +0530

    CHECKSUM test

--- Package.swift
+++ Package.swift
@@ -1,50 +1,10 @@
-// // swift-tools-version: 5.9
-// // swift-tools-version:5.9
-// import PackageDescription
-
-// let package = Package(
-//     name: "NailDiseaseSDK",
-//     platforms: [
-//         .iOS(.v13)  
-//     ],
-//     products: [
-//         .library(
-//             name: "NailDiseaseSDK",
-//             targets: ["NailDiseaseSDK"]
-//         ),
-//     ],
-//     dependencies: [
-//         // Add third-party dependencies here if needed
-//     ],
-//     targets: [
-//         .target(
-//             name: "NailDiseaseSDK",
-//             dependencies: [],
-//             path: "NailDiseaseSDK",
-//             exclude: [],  
-//             resources: [   
-//                 .process("model.tflite")  
-//             ]
-//         ),
-//         .testTarget(
-//             name: "NailDiseaseSDKTests",
-//             dependencies: ["NailDiseaseSDK"],
-//             path: "NailDiseaseSDKTests",
-//             exclude: [],
-//             resources: [
-//                 .process("TestImages/test_image.jpg")  
-//             ]
-//         ),
-//     ]
-// )
 // swift-tools-version: 5.9
-
 import PackageDescription
 
 let package = Package(
     name: "NailDiseaseSDK",
     platforms: [
-        .iOS(.v14) // Supports iOS 14 and above
+        .iOS(.v14)
     ],
     products: [
         .library(
@@ -56,7 +16,7 @@ let package = Package(
         .binaryTarget(
             name: "NailDiseaseSDK",
             url: "https://github.com/piumal96/iOS-SDK-Nail-Disease-Identification/releases/download/v1.0.0/NailDiseaseSDK.xcframework.zip",
-            checksum: "87e661cf8642f9a8d1ddaa17c71018a0bebc291ec9f9ea74c5700de96f764c17" // Auto-updated in CI
+            checksum: "PASTE_YOUR_CHECKSUM_HERE"
         )
     ]
 )
