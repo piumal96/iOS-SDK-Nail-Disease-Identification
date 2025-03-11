9ff3649c85446cd53786e155164553215e83135f
RMJP KUMARA <piumalkumara@live.com>
2025-03-11 19:35:45 +0530

    spm_test.swift

--- Package.swift
+++ Package.swift
@@ -1,11 +1,50 @@
+// // swift-tools-version: 5.9
+// // swift-tools-version:5.9
+// import PackageDescription
+
+// let package = Package(
+//     name: "NailDiseaseSDK",
+//     platforms: [
+//         .iOS(.v13)  
+//     ],
+//     products: [
+//         .library(
+//             name: "NailDiseaseSDK",
+//             targets: ["NailDiseaseSDK"]
+//         ),
+//     ],
+//     dependencies: [
+//         // Add third-party dependencies here if needed
+//     ],
+//     targets: [
+//         .target(
+//             name: "NailDiseaseSDK",
+//             dependencies: [],
+//             path: "NailDiseaseSDK",
+//             exclude: [],  
+//             resources: [   
+//                 .process("model.tflite")  
+//             ]
+//         ),
+//         .testTarget(
+//             name: "NailDiseaseSDKTests",
+//             dependencies: ["NailDiseaseSDK"],
+//             path: "NailDiseaseSDKTests",
+//             exclude: [],
+//             resources: [
+//                 .process("TestImages/test_image.jpg")  
+//             ]
+//         ),
+//     ]
+// )
 // swift-tools-version: 5.9
-// swift-tools-version:5.9
+
 import PackageDescription
 
 let package = Package(
     name: "NailDiseaseSDK",
     platforms: [
-        .iOS(.v13)  
+        .iOS(.v14) // Supports iOS 14 and above
     ],
     products: [
         .library(
@@ -13,27 +52,11 @@ let package = Package(
             targets: ["NailDiseaseSDK"]
         ),
     ],
-    dependencies: [
-        // Add third-party dependencies here if needed
-    ],
     targets: [
-        .target(
+        .binaryTarget(
             name: "NailDiseaseSDK",
-            dependencies: [],
-            path: "NailDiseaseSDK",
-            exclude: [],  
-            resources: [   
-                .process("model.tflite")  
-            ]
-        ),
-        .testTarget(
-            name: "NailDiseaseSDKTests",
-            dependencies: ["NailDiseaseSDK"],
-            path: "NailDiseaseSDKTests",
-            exclude: [],
-            resources: [
-                .process("TestImages/test_image.jpg")  
-            ]
-        ),
+            url: "https://github.com/piumal96/iOS-SDK-Nail-Disease-Identification/releases/download/v1.0.0/NailDiseaseSDK.xcframework.zip",
+            checksum: "YOUR_CHECKSUM_HERE" // Auto-updated in CI
+        )
     ]
 )
