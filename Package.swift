a1ee68a6346b3b26491da4fdb40a94e5d153cf79
RMJP KUMARA <piumalkumara@live.com>
2025-03-11 21:33:25 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .binaryTarget(
             name: "NailDiseaseSDK",
             url: "https://github.com/piumal96/iOS-SDK-Nail-Disease-Identification/releases/download/v1.0.3/NailDiseaseSDK.xcframework.zip",
-            checksum: "PASTE_a7867774ab59b1d25f77ede8e951f7507ac94cf1d44cd3b7bc49294f28d5cb93"
+            checksum: "PASTE_THE_ACTUAL_CHECKSUM_HERE"
         )
     ]
 )
