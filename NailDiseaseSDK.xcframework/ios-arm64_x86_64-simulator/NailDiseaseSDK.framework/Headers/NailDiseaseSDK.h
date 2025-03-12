10fc1e87e6f2991306e5a257559687e3a7963e2d
licensespring <piumalkumara@live.com>
2025-03-12 12:26:41 +0530

    Sandbox scrpit off

---
+++ NailDiseaseSDK.xcframework/ios-arm64_x86_64-simulator/NailDiseaseSDK.framework/Headers/NailDiseaseSDK.h
@@ -0,0 +1,18 @@
+//
+//  NailDiseaseSDK.h
+//  NailDiseaseSDK
+//
+//  Created by Piumal Kumara on 2025-02-12.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NailDiseaseSDK.
+FOUNDATION_EXPORT double NailDiseaseSDKVersionNumber;
+
+//! Project version string for NailDiseaseSDK.
+FOUNDATION_EXPORT const unsigned char NailDiseaseSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NailDiseaseSDK/PublicHeader.h>
+
+
